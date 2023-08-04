0ae7303d30627da71f581721e48e232fcbc2dbd9
Tonzi254 <danteskilz@gmail.com>
2023-08-04 18:11:40 +0300

    dilly dallying-commented code

--- arrays.js
+++ arrays.js
@@ -12,14 +12,14 @@
 // console.log(fruitsArray);
 
 // 4. Remove items at a specified position
-// fruitsArray.splice(1,1);
+// fruitsArray.splice(0,1);
 // console.log(fruitsArray);
 
 // 5. How to retrieve items given their position
 // console.log(fruitsArray[5]);
 
 // 6. Loops
-// Method 1
+// Method 1 // Betting Jackpot Winners Array. Send SMS to all the winners notifying them of their stakes
 // fruitsArray.forEach((item, index) => {
 //     if (item == 'Apple') {
 //         console.log('I don\'t like apples');
@@ -32,27 +32,27 @@ const fruitsArray = ['Mango', 'Apple', 'Banana', 'Watermelon', 'Grape'];
 // fruitsArray.map(function (value, index) {
 //     console.log(value, index);
 // })
-// fruitsArray.map((value) => {
-//     console.log(value);
+// fruitsArray.map((value, index) => {
+//     console.log(value, index);
 // });
 
-const newSubArray = fruitsArray.slice(1,5);
-console.log(newSubArray, fruitsArray);
+// const newSubArray = fruitsArray.slice(1,5);
+// console.log(newSubArray, fruitsArray);
 
-const splicedArray = fruitsArray.splice(1,2);
-console.log(splicedArray, fruitsArray);
+// const splicedArray = fruitsArray.splice(1,1);
+// console.log(splicedArray, fruitsArray);
 
 
-// Method 3
+// // Method 3
 for (let index = 0; index < fruitsArray.length; index++) {
     const element = fruitsArray[index];
     console.log(element,'is in position', index);
 }
 
-// Length vs Index
-const myWord = 'Fruits';
-for (let index = 0; index < myWord.length; index++) {
-    const singleLetter = myWord[index];
-    console.log(singleLetter)
-}
-console.log(myWord.length);
+// // Length vs Index
+// const myWord = 'Fruits';
+// for (let index = 0; index < myWord.length; index++) {
+//     const singleLetter = myWord[index];
+//     console.log(singleLetter)
+// }
+// console.log(myWord.length);
